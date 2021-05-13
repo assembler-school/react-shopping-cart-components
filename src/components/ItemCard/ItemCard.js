a2feaf0d722948d163e6b6b140ac65f6c19986f7
Nachomontoya <montoyanacho@gmail.com>
2021-05-13 18:41:53 +0200

    last commit

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -11,7 +11,19 @@ function Divider() {
   return <hr className="ItemCard__divider" />;
 }
 
-function ItemCard() {
+function ItemCard({
+  id,
+  img,
+  title,
+  shortDescription,
+  isFavorite,
+  upVotes,
+  downVotes,
+  handleDownVote,
+  handleUpVote,
+  handleSetFavorite,
+  handleAddToCart,
+}) {
   function onDownVote() {
     handleDownVote(id);
   }
@@ -25,7 +37,51 @@ function ItemCard() {
     handleAddToCart(id);
   }
 
-  return <article className="ItemCard col col-12 col-md-6 col-lg-4" />;
+  return (
+    <article className="ItemCard col col-12 col-md-6 col-lg-4">
+      <header>
+        <div className="ItemCard__image-wrapper">
+          <img src={img} className="ItemCard__image" alt={title} />
+          <FavoriteIconButton
+            handleSetFavorite={onSetFavorite}
+            isFavorite={isFavorite}
+            onClick={onSetFavorite}
+          />
+        </div>
+        <h2 className="ItemCard__title">{title}</h2>
+      </header>
+      <Divider />
+      <p className="ItemCard__description">{shortDescription}</p>
+      <Divider />
+      <footer className="ItemCard__meta">
+        <div className="ItemCard__icons">
+          <div className="ItemCard__icon-row">
+            <IconButton
+              aria-label="up vote product"
+              handleClick={onUpVote}
+              onClick={onUpVote}
+            >
+              <ThumbDown />
+            </IconButton>
+            <p className="ItemCard__icon-txt">{downVotes.currentValue}</p>
+          </div>
+          <div className="ItemCard__icon-row">
+            <IconButton
+              aria-label="down vote product"
+              handleClick={onDownVote}
+              onClick={onDownVote}
+            >
+              <ThumbUp />
+            </IconButton>
+            <p className="ItemCard__icon-txt">{upVotes.currentValue}</p>
+          </div>
+        </div>
+        <div className="ItemCard__icon-row">
+          <Button onClick={onAddToCart}>Add to cart</Button>
+        </div>
+      </footer>
+    </article>
+  );
 }
 
 export default ItemCard;
