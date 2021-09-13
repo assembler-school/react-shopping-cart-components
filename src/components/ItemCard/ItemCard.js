cb288264d932b6f73483080de9ca26fa8166599c
Daniella Alamo <dalamov08@gmail.com>
2021-09-13 16:18:40 +0200

    Update ItemCard.js

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -11,19 +11,19 @@ import "./ItemCard.scss";
     return <hr className="ItemCard__divider" />;
   }
 
-function ItemCard() {
-  id,
-  img,
-  title,
-  shortDescription,
-  isFavorite,
-  upVotes,
-  downVotes,
-  handleDownVote,
-  handleUpVote,
-  handleSetFavorite,
-  handleAddToCart,
-}
+  function ItemCard({
+    id,
+    img,
+    title,
+    shortDescription,
+    isFavorite,
+    upVotes,
+    downVotes,
+    handleDownVote,
+    handleUpVote,
+    handleSetFavorite,
+    handleAddToCart,
+  }) {
   function onDownVote() {
     handleDownVote(id);
   }
