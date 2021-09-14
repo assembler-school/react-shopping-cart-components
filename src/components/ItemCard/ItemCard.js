0275ff5b0b59f327f43a4bd69f3093cf87d2d334
Yaiser <yaiserar@gmail.com>
2021-09-14 20:35:41 +0200

    design pill required done

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -7,6 +7,10 @@ import FavoriteIconButton from "../FavoriteIconButton";
 
 import "./ItemCard.scss";
 
+function Divider() {
+  return <hr className="ItemCard__divider" />;
+}
+
 function ItemCard({
   id,
   img,
@@ -35,35 +39,39 @@ function ItemCard({
 
   return (
     <>
-      <div className="ItemCard">
-        <img src={img} className="ItemCard__image" alt={title} />
-        <FavoriteIconButton
-          handleSetFavorite={onSetFavorite}
-          isFavorite={isFavorite}
-        />
+      <article className="ItemCard col col-12 col-md-6 col-lg-4">
+        <header>
+          <div className="ItemCard__img-container">
+            <img src={img} className="ItemCard__image" alt={title} />
+            <FavoriteIconButton
+              handleSetFavorite={onSetFavorite}
+              isFavorite={isFavorite}
+            />
+          </div>
+        </header>
+        <h2>{title}</h2>
         <div className="ItemCard__content">
-          <h2>{title}</h2>
-          <hr />
+          <Divider />
           <p>{shortDescription}</p>
-          <hr />
-          <div className="actions">
-            <div className="votes">
+          <Divider />
+          <div className="ItemCard__actions">
+            <div className="ItemCard__votes">
               <IconButton aria-label="up vote product" handleClick={onUpVote}>
                 <ThumbUp />
-                <p>{upVotes.currentValue}</p>
               </IconButton>
+              <p>{upVotes.currentValue}</p>
               <IconButton
                 aria-label="down vote product"
                 handleClick={onDownVote}
               >
                 <ThumbDown />
-                <p>{downVotes.currentValue}</p>
               </IconButton>
+              <p>{downVotes.currentValue}</p>
             </div>
             <Button onClick={onAddToCart}>Add to cart</Button>
           </div>
         </div>
-      </div>
+      </article>
     </>
   );
 }
